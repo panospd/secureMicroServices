6e71599ad5bf501751a2837a47b1c46263ae1014
Panos Anastasiadis <anastapd@gmail.com>
2021-06-14 19:37:58 +0100

    Created movies api using in memory database and crud

---
+++ Movies.Api/Data/MoviesApiContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.Api.Model;
+
+namespace Movies.Api.Data
+{
+    public class MoviesApiContext : DbContext
+    {
+        public MoviesApiContext (DbContextOptions<MoviesApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
