73256becfab45ff67997edbe11f8ef9512fa49f9
Panos Anastasiadis <anastapd@gmail.com>
2021-06-15 14:29:14 +0100

    Implemented movies.client mvc app

---
+++ Movies.Client/ApiServices/IMovieApiService.cs
@@ -0,0 +1,15 @@
+﻿using Movies.Client.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Movies.Client.ApiServices
+{
+    public interface IMovieApiService
+    {
+        Task<IEnumerable<Movie>> GetMovies();
+        Task<Movie> GetMovie();
+        Task<Movie> CreateMovie();
+        Task<Movie> UpdateMovie();
+        Task DeleteMovie();
+    }
+}
---
+++ Movies.Client/ApiServices/MovieApiService.cs
@@ -0,0 +1,48 @@
+﻿using Movies.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Movies.Client.ApiServices
+{
+    public class MovieApiService : IMovieApiService
+    {
+        public async Task<IEnumerable<Movie>> GetMovies()
+        {
+            var moviesList = new List<Movie>();
+
+            moviesList.Add(new Movie
+            {
+                Id = 1,
+                Genre = "Comics",
+                Title = "asd",
+                Rating = "9.2",
+                ImageUrl = "images/src",
+                ReleaseDate = DateTime.Now,
+                Owner = "swn"
+            });
+
+            return await Task.FromResult(moviesList);
+        }
+
+        public Task<Movie> GetMovie()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Movie> CreateMovie()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteMovie()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Movie> UpdateMovie()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
