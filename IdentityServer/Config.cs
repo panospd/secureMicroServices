f38ccda4bb64d2b926dbc3f80378b7a8fa51d763
Panos Anastasiadis <anastapd@gmail.com>
2021-06-15 15:49:15 +0100

    Added identityServer quickstarter UI

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -1,9 +1,9 @@
-﻿using IdentityServer4.Models;
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
 using IdentityServer4.Test;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace IdentityServer
 {
@@ -21,6 +21,30 @@ namespace IdentityServer
                         new Secret("secret".Sha256())
                     },
                     AllowedScopes = { "movieAPI" }
+                },
+                new Client
+                {
+                    ClientId = "movies_mvc_client",
+                    ClientName = "Movies MVC Web App",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    AllowRememberConsent = false,
+                    RedirectUris = new List<string>
+                    {
+                        "https://localhost:5002/signin-oidc"
+                    },
+                    PostLogoutRedirectUris = new List<string>
+                    {
+                        "https://localhost:5002/signout-callback-oidc"
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
                 }
             };
 
@@ -39,13 +63,24 @@ namespace IdentityServer
         public static IEnumerable<IdentityResource> IdentityResources =>
             new IdentityResource[]
             {
-
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
             };
 
         public static List<TestUser> TestUsers =>
-            new List<TestUser>
+            new()
             {
-
+                new TestUser
+                {
+                    SubjectId = "9d2c859e-db44-462a-baaa-1a5a45932797",
+                    Username = "panos",
+                    Password = "panos",
+                    Claims = new List<Claim>
+                    {
+                        new Claim(JwtClaimTypes.GivenName, "panos"),
+                        new Claim(JwtClaimTypes.FamilyName, "ozkaya")
+                    },
+                }
             };
     }
 }
--- Movies.Client/Controllers/MoviesController.cs
+++ Movies.Client/Controllers/MoviesController.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using Movies.Client.ApiServices;
 using Movies.Client.Models;
 
 namespace Movies.Client.Controllers
 {
+    [Authorize]
     public class MoviesController : Controller
     {
         private readonly IMovieApiService _movieApiService;
@@ -18,7 +23,20 @@ namespace Movies.Client.Controllers
         // GET: Movies
         public async Task<IActionResult> Index()
         {
+            LogTokenAndClaims();
             return View(await _movieApiService.GetMovies());
+        }
+
+        public async Task LogTokenAndClaims()
+        {
+            var identityToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.IdToken);
+
+            Debug.WriteLine($"Identity token: {identityToken}");
+
+            foreach(var claim in User.Claims)
+            {
+                Debug.WriteLine($"Claim type: {claim.Type} - Claim value: {claim.Value}");
+            }
         }
 
         // GET: Movies/Details/5
